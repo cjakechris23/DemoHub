658dc2e7da28431bc980d60318eaae036d7566b1
Jake Chris Delos Reyes <39973112+cjakechris23@users.noreply.github.com>
2024-05-24 00:56:13 +0800

    PassingValuesFromParentToChild

--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,14 +1,14 @@
 import { LightningElement,track,api,wire } from 'lwc';
 import getBankAccounts from '@salesforce/apex/BankingAppController.getBankAccounts'
 export default class ParentComponent extends LightningElement {
-    firstName = '';
-    lastName = '';
-    value = 'from Parent';
+    Name = '';
+    Amount = '';
+    CreditCardNo = '';
+    @track recordId;
     @track account = [];
 
     handleEvent(event){
-        this.firstName = event.detail.firstName;
-        this.lastName = event.detail.lastName;
+        this.Name = event.detail.Name;
     }
 
     @wire(getBankAccounts)
@@ -17,6 +17,7 @@ export default class ParentComponent extends LightningElement {
             if(data){
                 this.account = data;
                 console.log(JSON.stringify(data));
+                this.recordId = data.Id
             }
             else if(error){
                 console.log('Error: ' , error);
